3ce685617508e75d8e539f10edfe75be5d0de1f6
Ram <ramagopalbyjava@gmail.com>
2023-04-08 23:45:20 +0530

    Filters applying drop down planName, planStatus and Gender

--- src/main/java/com/ashoikIt/Insurance/serviceImpl/RequestServiceImpl.java
+++ src/main/java/com/ashoikIt/Insurance/serviceImpl/RequestServiceImpl.java
@@ -2,7 +2,9 @@ package com.ashoikIt.Insurance.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.ashoikIt.Insurance.dto.SearchRequest;
@@ -11,38 +13,46 @@ import com.ashoikIt.Insurance.repo.CitizenPlanRepository;
 import com.ashoikIt.Insurance.service.ReportService;
 
 @Service
-public class RequestServiceImpl implements ReportService{
+public class RequestServiceImpl implements ReportService {
 
 	@Autowired
 	private CitizenPlanRepository planRepository;
-	
+
 	@Override
 	public List<String> getPlanName() {
-		
+
 		return planRepository.getCitizenPlanName();
 	}
 
 	@Override
 	public List<String> getPlanStatus() {
-		
+
 		return planRepository.getCitizenPlaNStatus();
 	}
 
 	@Override
 	public List<CitizenPlan> search(SearchRequest request) {
+		// filter applying
+		// using Dynamic Query using of Example of()
+
+		CitizenPlan entity = new CitizenPlan();
+		BeanUtils.copyProperties(request, entity);
+		return planRepository.findAll(Example.of(entity));
+		//Dates will not work because bugs there means diff formates
+		//Remaining will works three plan names ,plan status and gender
 		
-		return planRepository.findAll();
+		
 	}
 
 	@Override
 	public boolean exportExcel() {
-		
+
 		return false;
 	}
 
 	@Override
 	public boolean exportPdf() {
-		
+
 		return false;
 	}
 
